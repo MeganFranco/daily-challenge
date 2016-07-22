8aac575c15b5d38b1f25433016d53abc3c3d1aad
Megan Franco <francomegan@gmail.com>
2016-07-22 10:09:05 -0600

    temorary removal of audio

--- public/components/services/trackService.js
+++ public/components/services/trackService.js
@@ -2,17 +2,19 @@ var app = angular.module("RejectionApp");
 
 app.service("TrackerService", ["$http", "UserService", function ($http, UserService) {
     var self = this;
-//    var baseUrl = "http://localhost:5000/api/";
+    //    var baseUrl = "http://localhost:5000/api/";
 
 
     // Gets all the failure objects for a given user. Get by username
-//        return $http.get(baseUrl + 'failure')
+    //        return $http.get(baseUrl + 'failure')
+    this.get = function () {
         return $http.get('/api/failure')
             .then(function (response) {
-//            console.log('response ',response.data);
+                //            console.log('response ',response.data);
                 return response.data;
-                    //                self.failures = response.data;
-            });
+                //                self.failures = response.data;
+            })
+    };
 
     ////Adds failures to DB/////
     this.addFail = function (fail) {
